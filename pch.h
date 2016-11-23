15217d5d57bf6e9c16f3098cec446a9c2d9cde68
Nyiscsák Andris <nyufuna@gmail.com>
2016-11-23 22:43:13 +0100

    Added the prebuild google test libraries.

--- pch.h
+++ pch.h
@@ -1,6 +1,16 @@
 #pragma warning( push, 0 )
-#if __clang__
-#include "clang_gtest_helper.h"
+#if __clang__ && defined(GTEST) && GTEST == 1
+#define chdir		_chdir 
+#define fdopen		_fdopen
+#define read		_read
+#define write		_write
+#define close		_close
+#define dup			_dup
+#define dup2		_dup2
+#define creat		_creat
+
+#define O_RDONLY	_O_RDONLY
+#define O_APPEND	_O_APPEND
 #endif
 
 #include "gtest\gtest.h"
